568ad8ddbcf66fad1ad9805e2dcd81da84c39380
Dan S <github@danielsteinke.com>
2019-12-04 16:16:52 -0600

    Complete rewrite
    Complete rewrite, no more futzing with macOS Automator. Can now be compiled and run from a single app.

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
-# wordsquirt
- Python based text snippet expander for macos. My grandfather used to call writing implements, word squirters.
+# wordsquirt ✒️💦
+ Python based text snippet expander for macos. My grandfather used to call writing implements "word squirters". Written in Python for macOS.
 
 ## Python Package requirements (one day I'll learn to package my software):
-` TinyDB `
+
+```
+tinydb
+rumps
+pynput
+py2app
+```
 
 ## Sources:
-Based off this article:
-[https://chrisrosser.net/posts/2017/01/02/macos-system-wide-snippet-database/](Macos System Wide Snippet Database - chrisrosser.net)
-abc
+~~Based originally off this article:
+[https://chrisrosser.net/posts/2017/01/02/macos-system-wide-snippet-database/](Macos System Wide Snippet Database - chrisrosser.net)~~
+
+Current iteration is from scratch.  
 
 ## Set up
-Until I work out if/how I can register Automator services from a Python script, some assembly will be required:
-### wordsquirt automator config:
-![](doc/img/wordsquirt.png)
-### wordsuck automator config:
-![](doc/img/wordsuck.png)
-### Keyboard shortcut config (under services):
-![](doc/img/shortcuts.png)
+Compile with:
+
+``py2applet --make-setup wordsquirt/wordsquirt.py``
+
+Then run the resulting app. Since this program monitors and controls the keyboard you will need to give it Keyboard Monitoring and Accessibility permissions when macOS prompts. On my machine I get what appear to be errors about missing modules during build but an app is still produced and runs fine.
+
+Also in my experience, when using rumps and pynput together the app would crash when it would attempt to control the keyboard. Works as expected when compiled and run as a .app. On that note, I also had to delete and manually regrant Accessibility and Keyboard Monitoring whenever I made a new build. Otherwise the expansion wouldn't trigger, with no apparent errors.
+
+## Todo:
+- Implement drop down menu to delete existing expansions.
+- Make procedure for adding a new expansion more robust.
+- Possibly monitor the config and expansions file for updates to allow autoreload.
+- Add icon and other cosmetic features.
+- Add prompt to edit trigger string.
+- Add menu item displaying current trigger string.
+- Add menu to trigger expansions from dropdown.
--- wordsquirt/wordsquirt.py
+++ wordsquirt/wordsquirt.py
@@ -1,16 +1,81 @@
-from tinydb import TinyDB, Query
-from os.path import expanduser
-import sys
+from os import system, path
+import configparser
+import rumps
+from wsdaemon import WSDaemon
 
-DB_FILE_PATH = expanduser("~/.wordsquirt.json")
+CONFIG_LOCATION = path.expanduser("~/.wordsquirtconf")
+DEFAULT_OPTIONS = {
+'expansion_file' : path.expanduser("~/.wordsquirt"),
+'trigger_prefix' : ',,',
+'buffer_limit' : 10
+}
+APP_TITLE = "✒️💦"
+ADD_EX_LABEL = "Add New Text Expansion"
+EDIT_EX_LABEL = "Edit Expansion File"
+RELOAD_EX_LABEL = "Reload Expansion File"
+EDIT_CONF_LABEL = "Edit Config File"
 
-db_handle = TinyDB(DB_FILE_PATH)
+class WordSquirt(rumps.App):
+    def __init__(self):
+        super(WordSquirt, self).__init__(APP_TITLE)
+        self.config_obj = configparser.ConfigParser()
+        self.load_options()
+        self.menu = [ADD_EX_LABEL,EDIT_EX_LABEL, RELOAD_EX_LABEL,EDIT_CONF_LABEL]
 
-query_handle = Query()
+        self.wsdaemon = WSDaemon(self.config_obj['options'])
+        self.wsdaemon.kb_listener.start()
 
-try:
-    input_string = sys.argv[1]
-    query_result = db_handle.search(query_handle.trigger == input_string).pop()
-    print(query_result["text"])
-except IndexError:
-    pass
+    @rumps.clicked(ADD_EX_LABEL)
+    def add_expansion(self, _):
+        window = rumps.Window("Specify trigger shortcut (do not include prefix):", "Specify New Trigger")
+        first_prompt = window.run()
+        trigger = first_prompt.text
+        if not self.wsdaemon.validate_unique_trigger(trigger):
+            rumps.alert(f"Expansion with trigger {trigger} already exists!")
+            return False
+        elif len(trigger) > self.wsdaemon.buffer_limit:
+            rumps.alert(f"Trigger \"{trigger}\" too long, max length is {self.wsdaemon.buffer_limit} characters")
+            return False
+        window = rumps.Window(f"Specify expansion for {trigger}:", "Specify New Expansion")
+        second_prompt = window.run()
+        expansion = second_prompt.text
+        self.wsdaemon.save_expansion(trigger,expansion)
+
+    @rumps.clicked(EDIT_EX_LABEL)
+    def edit_expansions(self, _):
+        target = self.config_obj['options']['expansion_file']
+        system(f"open {target}")
+
+
+    @rumps.clicked(EDIT_CONF_LABEL)
+    def edit_preferences(self, _):
+        system(f"open {CONFIG_LOCATION}")
+
+    @rumps.clicked(RELOAD_EX_LABEL)
+    def reload_expansions(self, _):
+        self.wsdaemon.load_keywords()
+        rumps.alert("Expansions reloaded.")
+
+    def load_options(self):
+        if not path.exists(CONFIG_LOCATION):
+            self.set_option_defaults()
+        else:
+            self.config_obj.read(CONFIG_LOCATION)
+
+            must_save = False
+            for option in DEFAULT_OPTIONS:
+                if not self.config_obj.has_option('options', option):
+                    self.config_obj.set('options', option, str(DEFAULT_OPTIONS[option]))
+                    must_save = True
+            if must_save:
+                with open(CONFIG_LOCATION, 'w') as configfile:
+                    self.config_obj.write(configfile)
+
+    def set_option_defaults(self):
+        self.config_obj['options'] = DEFAULT_OPTIONS
+
+        with open(CONFIG_LOCATION, 'w') as configfile:
+            self.config_obj.write(configfile)
+
+if __name__ == "__main__":
+    WordSquirt().run()
